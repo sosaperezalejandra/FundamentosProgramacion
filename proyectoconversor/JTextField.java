3f28ba9ec48961502f69c611d98973ddc6e7d506
sosaperezalejandra <sossale133@gmail.com>
2025-12-06 20:23:01 -0600

    ProyectoConversor
    calculadora

---
+++ proyectoconversor/JTextField.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectoconversor;
+
+/**
+ *
+ * @author Aleso
+ */
+class JTextField {
+
+    void setText(String valueOf) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    Object getText() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
